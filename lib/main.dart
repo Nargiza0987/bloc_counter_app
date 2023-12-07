c05783571d1056da72606c46333dd5ce082732a7
Nargiza0987 <kulm1987@gmail.com>
2023-12-07 12:20:09 +0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,8 @@
+import 'package:bloc_counter_app/example1/app.dart';
+import 'package:bloc_counter_app/example2/app2.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
